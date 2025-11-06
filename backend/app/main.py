61f297c49e850a2bc35ef3bf4e6855e03a560d1a
anass17 <anassboutaib2018@gmail.com>
2025-11-06 17:59:11 +0100

    fix/Commented undefined routes

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,6 +4,6 @@ from app.routes import evaluation_routes, analysis_routes, recommendation_routes
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(evaluation_routes.bp)
-    app.register_blueprint(analysis_routes.bp)
-    app.register_blueprint(recommendation_routes.bp)
+    # app.register_blueprint(analysis_routes.bp)
+    # app.register_blueprint(recommendation_routes.bp)
     return app
