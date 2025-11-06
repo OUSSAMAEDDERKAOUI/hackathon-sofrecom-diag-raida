cb4a66621b3662ddf4a2204954364ca931948f09
A-007481D <labidabdelmalek@gmail.com>
2025-11-06 21:36:44 +0100

    feat: visualize the testing using streamlit as the temporary frontend

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,8 @@
 from app.main import create_app
+from flask_cors import CORS
 
 app = create_app()
+CORS(app)  # Enable CORS for all routes
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
